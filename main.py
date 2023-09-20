54b74affda7ba015ae3cf7d8b48a4119d6fd4f68
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-09-20 11:48:47 -0400

    File search
    Added function to search for the file

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import json
+import os
 
-file = open("C:/Users/gbell/OneDrive/Desktop/SNHU/Fall-2023/SDLC/Project/dbf84c87-9967-3dcc-857c-cb206907df92.json")
+#search for file and return full path
+def findFile(name,path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
 
+#start at users file for file search
+path = "C:\\Users\\"
+fileName = "dbf84c87-9967-3dcc-857c-cb206907df92.json"
+
+# get the full path to the file
+fullFilePath = findFile(fileName, path)
+
+#open the file and load data to dictionary
+file = open(fullFilePath)
 minecraftData = json.load(file)
 
+#print all data
 for i in minecraftData:
    print(i)
+
+
 
 #Find out what stats we want to display and pull those into variables
 #Push those stats to DB
