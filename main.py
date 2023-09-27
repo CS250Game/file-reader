d9cc089e33b1100c49909e80a5f0ca91278fd845
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-09-27 11:43:08 -0400

    Added postgres connection setup
    installed psycopg2 postgres connection package and import library to setup db connection

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import json
 import os
+import psycopg2
+
+
+#connect to database to retrieve & send data
+#https://www.postgresqltutorial.com/postgresql-python/connect/
+
+#set up db connection, needs actual db info 
+#Connection should ideally be made in a config file
+dbConnection = psycopg2.connect(
+    host="localhost",
+    database="suppliers",
+    user="postgres",
+    password="Abcd1234")
+
+
 
 #search for file and return full path
 def findFile(name,path):
@@ -8,8 +23,10 @@ def findFile(name,path):
             return os.path.join(root, name)
 
 #start at users file for file search
+UID = "dbf84c87-9967-3dcc-857c-cb206907df92"
+
 path = "C:\\Users\\"
-fileName = "dbf84c87-9967-3dcc-857c-cb206907df92.json"
+fileName = UID + ".json"
 
 # get the full path to the file
 fullFilePath = findFile(fileName, path)
@@ -18,11 +35,30 @@ fullFilePath = findFile(fileName, path)
 file = open(fullFilePath)
 minecraftData = json.load(file)
 
+
 #print all data
 for i in minecraftData:
    print(i)
 
 
 
+#get file with most recent save
+#os.path.getctime(path)
+
+
 #Find out what stats we want to display and pull those into variables
 #Push those stats to DB
+
+#stat.walkOneCm
+#stat.killEntity.Chicken
+#stat.swimOneCm
+#stat.climbOneCm
+#stat.flyOneCm
+#stat.timeSinceDeath
+#stat.sprintOneCm
+#stat.playOneMinute
+#stat.killEntity.Pig
+#stat.killEntity.Cow
+#stat.damageDealt
+#stat.killEntity.Sheep
+#stat.killEntity.Donkey
