2c22e2e5da5e5641e8c5f00be843a98eb25fd907
averyB <averwhy@gmail.com>
2023-10-18 12:09:58 -0400

    Add systray usage, and base class layout

--- main.py
+++ main.py
@@ -1,80 +1,83 @@
 import json
 import os
 import psycopg2
-from configparser import ConfigParser
+import typing
+import pystray
+from PIL import Image
+
+class StatsFileNotFound(Exception):
+    """Base error for a statistic file that was not found."""
+    pass
+
+class StatNotFound(Exception):
+    """Base error for a statistic that was not found within a stat file."""
+    pass
+
+class StatisticsFile:
+    def __init__(self, uuid: str, world_name: str) -> None:
+        self.uuid = uuid
+        self.file_name = f"{self.uuid}.json".strip()
+        self.file_path = None
+        self.world_name = world_name
+
+        #File searching logic goes here
+        
+    def get_stat(self, name: str) -> dict:
+        """Gets a statistic from value name."""
+        pass
+
+    def search_stat(self, value: typing.Union[int, tuple])-> dict:
+        """Searches the stats file for stats with values matching the specified
+        number or multiple within the range.
+        `value` can either be a int or tuple."""
+        pass
+
+    @staticmethod
+    def find_uuid() -> str:
+        """Finds a UUID from a worldname."""
+        pass
+
+    @staticmethod
+    def wait_for_new_world() -> tuple:
+        """Waits for the creation of a new world. Once found, this will
+        return the world name and UUID of the stats file in a tuple."""
+        pass
+
+class Database:
+    def __init__(self, username:str, password:str, host:str, port:int, database:str = 'mttesting') -> None:
+        dsn = f"postgres://{username}:{password}@{host}:{port}/{database}"
+        self.conn = psycopg2.connect(dsn=dsn)
+
+    def push(self, stat_file: StatisticsFile):
+        pass
 
 
+#System tray icon stuff
+image = Image.open("img/Minetraxbackground.png")
+queries = [
+    'Settings',
+    'Account',
+    'Tracking world:',
+    'Exit'
+]
+def after_click(icon, query):
+    if str(query) == queries[0]:
+        print("s")
+        # icon.stop()
+    elif str(query) == queries[1]:
+        print("a")
+        # icon.stop()
+    elif str(query) == queries[2]:
+        print("t")
+    elif str(query) == queries[3]:
+        icon.stop()
 
-#downloading postgres to test connection
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
+def setup(): # Put object definitions and other code in here
+    icon = pystray.Icon("MT", image, "Minetrax", menu=pystray.Menu(
+    pystray.MenuItem(queries[0], after_click),
+    pystray.MenuItem(queries[1], after_click),
+    pystray.MenuItem(queries[2], after_click),
+    pystray.MenuItem(queries[3], after_click)))
+    icon.run()
 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
-
-#connect to database to retrieve & send data
-#https://www.postgresqltutorial.com/postgresql-python/connect/
-
-#set up db connection, needs actual db info 
-#Connection should ideally be made in a config file
-dbConnection = config()
-
-
-
-#search for file and return full path
-def findFile(name,path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-
-#start at users file for file search
-UID = "dbf84c87-9967-3dcc-857c-cb206907df92"
-
-path = "C:\\Users\\"
-fileName = UID + ".json"
-
-# get the full path to the file
-fullFilePath = findFile(fileName, path)
-
-#open the file and load data to dictionary
-file = open(fullFilePath)
-minecraftData = json.load(file)
-
-
-#print all data
-for i in minecraftData:
-   print(i)
-
-
-
-#get file with most recent save
-#os.path.getctime(path)
-
-
-#Find out what stats we want to display and pull those into variables
-#Push those stats to DB
-
-#stat.walkOneCm
-#stat.killEntity.Chicken
-#stat.swimOneCm
-#stat.climbOneCm
-#stat.flyOneCm
-#stat.timeSinceDeath
-#stat.sprintOneCm
-#stat.playOneMinute
-#stat.killEntity.Pig
-#stat.killEntity.Cow
-#stat.damageDealt
-#stat.killEntity.Sheep
-#stat.killEntity.Donkey
+setup()
