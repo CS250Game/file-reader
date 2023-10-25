5c314b3c196e1aca7f57f43988a6983af5839945
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-10-25 11:50:09 -0400

    Deleted config()

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from configparser import ConfigParser
 import json
 import os
 import psycopg2
@@ -139,31 +138,6 @@ def setup(): # Put object definitions and other code in here
     icon.run()
 
 setup()
-
-#downloading postgres to test connection
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
-
-#connect to database to retrieve & send data
-#https://www.postgresqltutorial.com/postgresql-python/connect/
-
-#set up db connection, needs actual db info 
-#Connection should ideally be made in a config file
-dbConnection = config()
 
 
            
